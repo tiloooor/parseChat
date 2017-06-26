eba5554420fd39c8343d287047903e423a8f84cf
Tyler Holloway <htoyll@fb.com>
2017-06-26 11:47:15 -0700

    completed registration and login functions

--- parseChat/LoginViewController.swift
+++ parseChat/LoginViewController.swift
@@ -27,11 +27,39 @@ class LoginViewController: UIViewController {
     
     //creates action for signup
     @IBAction func pressSignUp(_ sender: Any) {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = userNameTextField.text
+        newUser.password = passwordTextField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
     }
     
     
     //creates action for login
     @IBAction func pressLogin(_ sender: Any) {
+        //if nill set to default, which is an empty string
+        let username = userNameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }
     }
     
 
