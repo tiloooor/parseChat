0f3c10d2a8b010d41e941722e07e0f68283679f8
Katie Jiang <katiejiang@fb.com>
2017-06-26 13:24:32 -0700

    save chat messages when send button pressed

--- parseChat/ChatViewController.swift
+++ parseChat/ChatViewController.swift
@@ -7,8 +7,24 @@
 //
 
 import UIKit
+import Parse
 
 class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var chatMessageField: UITextField!
+    
+    @IBAction func pressSend(_ sender: Any) {
+        let chatMessage = PFObject(className: "Message_fbuJuly2017")
+        chatMessage["text"] = chatMessageField.text ?? ""
+        chatMessage.saveInBackground { (success, error) in
+            if success {
+                print("the message was saved")
+                self.chatMessageField.text = ""
+            } else if let error = error {
+                print("problem saving the message: \(error.localizedDescription)")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
